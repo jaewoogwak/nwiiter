974aaa0a5c6ed6251abcbb0d225d0fdf5f165229
gwak <jaewoo010207@gmail.com>
2021-12-09 00:11:17 +0900

    lec #2.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import AppRouter from "components/Router";
-import {useState} from 'react';
-import {authService} from 'fbase';
+import { useState } from "react";
+import { authService } from "fbase";
 
 function App() {
-  console.log(authService.currentUser)
+  console.log(authService.currentUser);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
-      <AppRouter isLoggedIn = {isLoggedIn}/>
+      <AppRouter isLoggedIn={isLoggedIn} />
       <footer> &copy; {new Date().getFullYear()} Nwitter </footer>
-
     </>
   );
 }
