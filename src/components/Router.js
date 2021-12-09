860a332b6d6a8fc041b632cb56130c5b3494891d
gwak <jaewoo010207@gmail.com>
2021-12-10 01:00:57 +0900

    lec #2.6 remain

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,23 +3,27 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
+import Test from "routes/Test";
 
-const AppRouter = ({isLoggedIn}) => {
-    return (
-        <Router>
-            <Routes>
-                {isLoggedIn ? (
-                    <>
-                        <Route exact={true} path={"/"} element={<Home />}>
-                        </Route>
-                    </>
-                ) : (
-                    <Route exact={true} path={"/"} element={<Auth />}>
-                    </Route>
-                )}
-            </Routes>
-        </Router>
-    )
-}
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route exact={true} path={"/"} element={<Home />}></Route>
+            <Route exact={true} path={"/profile"} element={<Profile />}></Route>
+            <Route exatc={true} path={"/test"} element={<Test />}></Route>
+          </>
+        ) : (
+          <Route exact={true} path={"/"} element={<Auth />}></Route>
+        )}
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => <div>Log In Success!</div>;
 export default Home;
